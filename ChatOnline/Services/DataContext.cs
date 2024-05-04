b9248ac777cadad30754c38efb69f8ef60926faa
Raimexd <nn8167722@gmail.com>
2024-05-04 09:25:51 +0300

    Добавьте файлы проекта.

---
+++ ChatOnline/Services/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using ChatOnline.Data;
+
+namespace ChatOnline.Services
+{
+    public class DataContext
+    {
+        public List<Chat> Chats = new List<Chat>();
+
+        public List<User> Users = new List<User>()
+        {
+            new User("Login1", "talgat"),
+            new User("Login2", "Password2"),
+            new User("Login3", "Password3")
+        };
+    }
+}
